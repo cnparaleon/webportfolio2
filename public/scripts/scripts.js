4233a9917aa981f14dbf9462c4474bcaa2f8c661
carlos.paraleon <carlos.paraleon@gmail.com>
2024-09-06 02:25:20 +0800

    Initial commit

---
+++ public/scripts/scripts.js
@@ -0,0 +1,18 @@
+// Nav Bar on scroll (not implemented)    
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-80px";
+//   }
+// }
+
+
+//This is to initialize the tooltip which is using jquerry
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
